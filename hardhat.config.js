ffd341b9eb8923f7189bbc8b38150c5bdd38c118
Andrew <andrewtarantino1432@gmail.com>
2023-11-05 17:14:08 -0700

    3. Testing Contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.9",
 };
--- test/Token.js
+++ test/Token.js
@@ -1,15 +1,40 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
 describe('Token', () => {
-  
-  it('has a name', async () => {
-	// Fetch Token from Blockchain
+  let token
+
+  beforeEach(async () => {
     const Token = await ethers.getContractFactory('Token')
-    let token = await Token.deploy()
-	// Read token name
-	const name = await token.name()
-	// Check that name is correct
-	expect(name).to.equal('My Token')
-  }) 
+    token = await Token.deploy('Protein', 'PRO', '1000000')
+  })
+
+  describe('Deployment', () => {
+    const name = 'Protein'
+    const symbol = 'PRO'
+    const decimals = '18'
+    const totalSupply = tokens('1000000')
+
+    it('has correct name', async () => {
+      expect(await token.name()).to.equal(name)
+    }) 
+
+    it('has correct symbol', async () => {
+      expect(await token.symbol()).to.equal(symbol)
+    }) 
+ 
+    it('has correct decimals', async () => {
+      expect(await token.decimals()).to.equal(decimals)
+    })
+
+    it('has correct total supply', async () => {
+      expect(await token.totalSupply()).to.equal(totalSupply)
+    })
+
+  })
+
 })
