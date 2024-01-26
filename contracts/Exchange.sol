05f90c6a170b1d18a8c77d3e38a5218eda4d001f
Andrew <andrewtarantino1432@gmail.com>
2024-01-25 22:19:45 -0700

    11. Make Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,14 +8,39 @@ contract Exchange {
 	address public feeAccount;
 	uint256 public feePercent;
 	mapping(address => mapping(address => uint256)) public tokens;
+	mapping(uint256 => _Order) public orders;
+	uint256 public orderCount;
 
-	event Deposit(address token, address user, uint256 amount, uint256 balance);
+	event Deposit(
+		address token,
+		address user,
+		uint256 amount,
+		uint256 balance
+	);
 	event Withdraw(
 		address token,
 		address user,
 		uint256 amount,
 		uint256 balance
 	);
+	event Order(
+		uint256 id,
+		address user,
+		address tokenGet,
+		uint256 amountGet,
+		address tokenGive,
+		uint256 amountGive,
+		uint256 timestamp
+	);
+	struct _Order {
+		uint256 id;
+		address user;
+		address tokenGet;
+		uint256 amountGet;
+		address _tokenGive;
+		uint256 amountGive;
+		uint256 timestamp;
+	}
 
 	constructor (address _feeAccount, uint256 _feePercent) {
 		feeAccount = _feeAccount;
@@ -51,6 +76,37 @@ contract Exchange {
 		returns (uint256)
 	{
 		return tokens[_token][_user];
+	}
+
+	function makeOrder(
+		address _tokenGet,
+	    uint256 _amountGet,
+	    address _tokenGive,
+	    uint256 _amountGive
+	) public {
+
+		require(balanceOf(_tokenGive, msg.sender) >=  _amountGive);
+
+		orderCount = orderCount + 1;
+		orders[orderCount] = _Order(
+			orderCount,
+			msg.sender,
+			_tokenGet,
+			_amountGet,
+			_tokenGive,
+			_amountGive,
+			block.timestamp
+		);
+		
+		emit Order(
+			orderCount,
+			msg.sender,
+			_tokenGet,
+			_amountGet,
+			_tokenGive,
+			_amountGive,
+			block.timestamp
+		);
 	}
 
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -15,6 +15,7 @@ describe('Exchange', () => {
     const Token = await ethers.getContractFactory('Token')
  
     token1 = await Token.deploy('Protein', 'PRO', '1000000')
+    token2 = await Token.deploy('Mock Dai', 'mDAI', '1000000')
 
     accounts = await ethers.getSigners()
     deployer = accounts [0]
@@ -137,6 +138,48 @@ describe('Exchange', () => {
   
     }) 
 
-  })
+  describe ('Making orders', async () => {
+    let transaction, result
 
+    let amount = tokens(1)
+
+    describe('Success', async () => {
+      beforeEach(async () => {
+        transaction = await token1.connect(user1).approve(exchange.address, amount)
+        result = await transaction.wait()
+       
+        transaction = await exchange.connect(user1).depositToken(token1.address, amount)
+        result = await transaction.wait()
+        
+        transaction = await exchange.connect(user1).makeOrder(token2.address, amount, token1.address, amount)
+        result = await transaction.wait()
+      })
+      
+      it('tracks the newly created oreder', async () => {
+        expect(await exchange.orderCount()).to.equal(1)
+      })  
+      
+      it('emits an Order event', async () => {
+        const event = result.events[0]
+        expect(event.event).to.equal('Order')
+
+        const args = event.args
+        expect(args.id).to.equal(1)
+        expect(args.user).to.equal(user1.address)
+        expect(args.tokenGet).to.equal(token2.address)
+        expect(args.amountGet).to.equal(tokens(1))
+        expect(args.tokenGive).to.equal(token1.address)
+        expect(args.amountGive).to.equal(tokens(1))
+        expect(args.timestamp).to.at.least(1)
+      })
+    
+    })
+      
+    describe('Failure', async () => {
+      it('Rejects with no balances', async () => {
+        await expect(exchange.connect(user1).makeOrder(token2.address, tokens(1), token1.address, tokens(1))).to.be.reverted
+      })
+    })
+  })
+})
 })
