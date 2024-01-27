69c324f8d978fca929739e91ddaf7ecab0308e4c
Andrew <andrewtarantino1432@gmail.com>
2024-01-26 17:02:29 -0700

    11. Cancel orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ contract Exchange {
 	mapping(address => mapping(address => uint256)) public tokens;
 	mapping(uint256 => _Order) public orders;
 	uint256 public orderCount;
+	mapping(uint256 => bool) public orderCancelled;
 
 	event Deposit(
 		address token,
@@ -32,12 +33,23 @@ contract Exchange {
 		uint256 amountGive,
 		uint256 timestamp
 	);
+	event Cancel(
+		uint256 id,
+		address user,
+		address tokenGet,
+		uint256 amountGet,
+		address tokenGive,
+		uint256 amountGive,
+		uint256 timestamp
+	);
+	
+
 	struct _Order {
 		uint256 id;
 		address user;
 		address tokenGet;
 		uint256 amountGet;
-		address _tokenGive;
+		address tokenGive;
 		uint256 amountGive;
 		uint256 timestamp;
 	}
@@ -105,6 +117,26 @@ contract Exchange {
 			_amountGet,
 			_tokenGive,
 			_amountGive,
+			block.timestamp
+		);
+	}
+
+	function cancelOrder(uint256 _id) public {
+		_Order storage _order = orders[_id];
+		
+		orderCancelled[_id] = true;
+		
+		require(address(_order.user) == msg.sender);
+
+		require(_order.id == _id);
+
+		emit Cancel(
+			_order.id,
+		    msg.sender,
+			_order.tokenGet,
+			_order.amountGet,
+			_order.tokenGive,
+			_order.amountGive,
 			block.timestamp
 		);
 	}
