24e4ec30b347ede8fe975caf6233babff07256b5
Andrew <andrewtarantino1432@gmail.com>
2024-02-07 12:25:06 -0700

    21. Deposits Pt 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
     <div className='exchange__header grid'>
       <div className='exchange__header--brand flex'>
         <img src={logo} className="logo" alt="PRO logo"></img>
-        <h1>PRO Token Exchange</h1>
+        <h1>PRO Exchange</h1>
       </div>
 
       <div className='exchange__header--networks flex'>
